6b07bec8058a80b4961c81eee77b8f18a8b82178
Your Name <you@example.com>
2022-11-04 16:00:08 -0400

    Remove All Submodules

--- src/EgxEngine/cpputility.cpp
+++ src/EgxEngine/cpputility.cpp
@@ -1,2 +1,4 @@
 #define CPP_UTILITY_IMPLEMENTATION
 #include <Utility/CppUtility.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
--- submodules/setup.bat
+++ submodules/setup.bat
@@ -1,6 +1,11 @@
 @echo off
 git clone https://github.com/youss2017/Utility
 git clone https://github.com/ocornut/imgui
+git clone https://github.com/nothings/stb
+git clone https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator
+git clone https://github.com/g-truc/glm
+git clone https://github.com/assimp/assimp
+git clone https://github.com/glfw/glfw
 echo Switching to docking branch of ImGui
 cd imgui
 git checkout docking
