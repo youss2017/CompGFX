51505f8e0fc22029fc6fcba0538c316d731a02c0
Your Name <you@example.com>
2022-11-04 15:50:12 -0400

    Remove Stb

--- submodules/setup.bat
+++ submodules/setup.bat
@@ -3,6 +3,8 @@ git clone https://github.com/youss2017/Utility
 git clone https://github.com/ocornut/imgui
 echo Switching to docking branch of ImGui
 cd imgui
-git switch -C docking
+git checkout docking
+git branch --set-upstream-to=origin/docking docking
+git pull
 cd ..
 echo Done
